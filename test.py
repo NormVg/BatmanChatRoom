26d5b76782fa8f4519d00f2409c26d2dc407d3a5
vishnu gupta <thenormvg@gmail.com>
2023-11-03 15:29:41 +0530

    added requrements

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ db =  TinyDB("./db/db.json")
 
 person = tinydb.Query()
 
-# db.insert({'username': 'NormVg', 'password': "08b63d8ade1bd145200a3aeced242edceb0180cec322ec26d7997932b1315025"})
+db.insert({'username': 'MrPixel', 'password': "12f0a4ba22f08d7499fc7def1c98c680ac2c99c67fc7ba2cbcc5ec2f6a8c6a16"})
 # db.insert({'username': 'vishnu_gupta_vg', 'password': "ab6b786aa204199a39492078eab36895cfa6b650d969c79f85a0055d461a0c52"})
 print(db.all())
 
